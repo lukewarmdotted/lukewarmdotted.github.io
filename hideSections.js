4cbc2248cbcf2f757b13fc15d905ff013caacfed
stutommi <tommi.teetee@hotmail.com>
2021-03-22 19:30:47 +0200

    initial commit

---
+++ hideSections.js
@@ -0,0 +1,11 @@
+const bMobile =   // will be true if running on a mobile device
+  navigator.userAgent.indexOf("Mobile") !== -1 ||
+  navigator.userAgent.indexOf("iPhone") !== -1 ||
+  navigator.userAgent.indexOf("Android") !== -1 ||
+  navigator.userAgent.indexOf("Windows Phone") !== -1
+
+if (!bMobile) {
+  const sheet = document.createElement("style")
+  sheet.innerHTML = ".hide-js {visibility: hidden}"
+  document.body.appendChild(sheet)
+}
---
+++ scrollReveal.js
@@ -0,0 +1,44 @@
+const slideUpSlowO = {
+  distance: '5%',
+  origin: 'bottom',
+  opacity: 0,
+  mobile: false,
+  duration: 1000
+}
+
+const slideUpSlow = {
+  distance: '5%',
+  opacity: 1,
+  origin: 'bottom',
+  mobile: false,
+  duration: 1000
+}
+
+const slideDownSlow = {
+  scale: 0.99,
+  distance: '10%',
+  origin: 'top',
+  opacity: 0,
+  mobile: false,
+  duration: 2000
+}
+
+const slideLeft = {
+  distance: '20%',
+  origin: 'left',
+  mobile: false,
+  duration: 1000
+}
+
+const slideDownFast = {
+  distance: '20%',
+  origin: 'top',
+  mobile: false,
+  duration: 1000
+}
+
+ScrollReveal().reveal('.slide-up-slow-o', slideUpSlowO)
+ScrollReveal().reveal('.slide-up-slow', slideUpSlow)
+ScrollReveal().reveal('.slide-down-slow', slideDownSlow)
+ScrollReveal().reveal('.slide-left', slideLeft)
+ScrollReveal().reveal('.slide-down-fast', slideDownFast)
